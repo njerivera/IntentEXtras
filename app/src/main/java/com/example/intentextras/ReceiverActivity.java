af71fc3b510293bd2d305e98323f39e4754acb14
njerivera <veronjeri85@gmail.com>
2019-07-01 16:54:18 +0300

    added intent

---
+++ app/src/main/java/com/example/intentextras/ReceiverActivity.java
@@ -0,0 +1,38 @@
+package com.example.intentextras;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class ReceiverActivity extends AppCompatActivity{
+  TextView tvName;
+  TextView tvAge;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receiver);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        tvName = findViewById(R.id.tvName);
+        tvAge = findViewById((R.id.tvAge));
+
+
+
+
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+}
